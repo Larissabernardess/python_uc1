692120b5f7766891aae8118d4aa2d3564992e98b
Larissabernardess <lbbbernardes@gmail.com>
2025-03-12 11:48:15 -0300

    Add files via upload

---
+++ aula03desafio4.3.py
@@ -0,0 +1,27 @@
+numero =int(input('Escolha um número inteiro:'))
+
+print(f'O número inicial é:{numero}')
+
+num = numero
+
+num+=7
+print(f'A adição é:{num}')
+
+num-=4
+print(f'A subtração é:{num}')
+
+num*=3
+print(f' A multiplicação é;{num}')
+
+num/=4
+print(f'A divição é:{num}')
+
+num//=3
+print(f'A divição inteira é:{num}')s
+
+num%=2
+print(f'O módulo é:{num}')
+
+num**=3
+print(f'A exponenciação é:{num}')
+
