3a894a4e83a1ca681bd97e33bbe4228570f7ce96
Larissabernardess <lbbbernardes@gmail.com>
2025-03-14 20:32:43 -0300

    Add files via upload

---
+++ aula-05-desafio-01.py
@@ -0,0 +1,9 @@
+usuário =int(input('informe a sua idade: '))
+senha =float(input('Informe sua senha: '))
+
+if (usuário > 18) and (senha==12345):
+    print('ACESSO PERMITIDO')
+
+else:
+    print('ACESSO NEGADO')
+    
