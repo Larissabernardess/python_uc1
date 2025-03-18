7dc9911a958175e08649d20ce6ebb8ff3866e65a
Larissabernardess <lbbbernardes@gmail.com>
2025-03-18 08:49:57 -0300

    Add files via upload

---
+++ aula04_desafio04_01.py
@@ -0,0 +1,13 @@
+numero=float(input('Escolha um numero: '))
+if numero> 10:
+    print('O número é possitivo')
+
+elif numero< 9:
+    print('Onumero é negativo')
+
+else:
+    print('O número é zero')
+    
+
+
+
